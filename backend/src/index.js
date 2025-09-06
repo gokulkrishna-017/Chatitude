114e1d4e1902f8c8326e211cba9144ed55ab1d44
Gokul Krishna <gokulkrishna1117@gmail.com>
2025-09-06 19:55:04 +0530

    image upload change

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,8 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({limit: "10mb", extended: true}));
 app.use(
   cors({
     origin: "http://localhost:5173",
